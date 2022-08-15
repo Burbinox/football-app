7c185d776fb1fc34b5f86540b1fad50940e98f92
Burbinox <hubert.grobelny@interia.pl>
2022-08-15 20:13:56 +0200

    add dragable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
 import pitch from './static/pitch.png';
+import Draggable from 'react-draggable';
 
 function App() {
   function addPlayer() {
+    console.log(1)
     return (
-      <div>
-        JESTEM STWORZONY HAHAHAHAHAH
-      </div>
+      <Draggable>
+        <div>I can now be moved around!</div>
+      </Draggable>
     )
   }
 
   return (
     <div className='Main'>
-      <img src={pitch} alt="Pitch" class="Pitch"/>
+      <img src={pitch} alt="Pitch" className="Pitch"/>
       <div className='Menu'>
         <button type="button" onClick={addPlayer}>Add our player</button>
       </div>
     </div>
+    
   );
 }
 
